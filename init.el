839ef83e0d4fc7c41ab145ed6d3ff8d40099c569
Cody Harris <x0ul@users.noreply.github.com>
2024-09-13 13:02:40 -0700

    cleanup, add elixir

--- init.el
+++ init.el
@@ -6,8 +6,13 @@
 (setq inhibit-startup-message t
       inhibit-startup-echo-area-message t)
 (column-number-mode)
-(load-theme 'wombat)
-;; TODO look into color-theme to share this?
+
+(use-package wombat-theme  ;; Optional to manage via use-package
+  :init (load-theme 'wombat t))
+
+;; (load-theme 'wombat)
+
+;; Custom face settings
 (set-face-background 'default "#111")
 (set-face-background 'cursor "#c96")
 (set-face-background 'isearch "#c60")
@@ -21,9 +26,16 @@
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
         ("gnu" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+;; (unless package-archive-contents
+;;   (package-refresh-contents))
+
+;; Install use-package if not already installed
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+;; Load use-package for package management
+(eval-when-compile
+  (require 'use-package))
 
 ;; Keep `custom' from dirtying up .emacs / init.el
 (setq custom-file "~/.emacs.d/custom.el")
@@ -35,13 +47,107 @@
 (set-default 'truncate-lines t)
 
 ;; Better defaults!
-(use-package better-defaults)
+(use-package better-defaults
+  :ensure t)
 ;; ... but let's actually show menus
 (menu-bar-mode 1)
 
+;; smartparens
+(use-package smartparens
+  :ensure t
+  :config
+  (require 'smartparens-config))
+
+(require 'loadhist)
+(file-dependents (feature-file 'cl))
+
 ;; Flycheck, needed for python and others?
-(use-package flycheck)
-(add-hook 'elpy-mode-hook 'flycheck-mode)
+(use-package flycheck
+  :ensure t
+  :hook (prog-mode . flycheck-mode))
+;; (add-hook 'elpy-mode-hook 'flycheck-mode)
+
+;; Let elixir-ts-mode start it, otherwise problems
+;; ;; Tree-sitter configuration
+;; (use-package tree-sitter
+;;  :ensure t
+;;  :hook (prog-mode . tree-sitter-mode)
+;;  :config
+;;  (use-package tree-sitter-langs
+;;    :ensure t))
+
+;; Elixir
+(defun ensure-elixir-ts-grammar ()
+  "Ensure that the Elixir Treesitter grammar is installed."
+  (unless (file-exists-p (expand-file-name "libtree-sitter-elixir.so" (concat (expand-file-name user-emacs-directory) "tree-sitter")))
+    (message "Elixir Treesitter grammar not found. Installing...")
+    (elixir-ts-install-grammar)))
+
+(use-package elixir-ts-mode
+  :ensure t
+  :hook
+  ((elixir-ts-mode . flycheck-mode)
+   (elixir-ts-mode . smartparens-mode)
+   (elixir-ts-mode . mix-minor-mode)
+   (elixir-ts-mode . exunit-mode)
+   (elixir-ts-mode . ensure-elixir-ts-grammar))
+  :config
+  (add-to-list 'auto-mode-alist '("\\.exs?\\'" . elixir-ts-mode))
+  (add-hook 'elixir-ts-mode-hook (lambda ()
+                                           (add-hook 'before-save-hook 'lsp-format-buffer))))
+(use-package
+  flycheck-credo
+  :ensure t
+  :after (flycheck elixir-ts-mode)
+  :config
+  (flycheck-credo-setup)
+  :custom (flycheck-elixir-credo-strict t))
+
+(use-package
+  exunit
+  :ensure t
+  :after elixir-ts-mode
+  :bind
+    (:map elixir-ts-mode-map
+        ("C-c , a" . exunit-verify-all)
+        ("C-c , A" . exunit-verify-all-in-umbrella)
+        ("C-c , s" . exunit-verify-single)
+        ("C-c , v" . exunit-verify)
+        ("C-c , r" . exunit-rerun)
+        ("C-c , t" . exunit-toggle-file-and-test)
+        ("s-r" . exunit-rerun)
+        ))
+
+(use-package
+  mix
+  :ensure t
+  :hook (elixir-ts-mode . mix-minor-mode)
+  :after elixir-ts-mode)
+
+(use-package lsp-mode
+  :ensure t
+  :hook (elixir-ts-mode . lsp)
+  :diminish lsp-mode
+  :commands lsp)
+
+(use-package lsp-ui
+  :ensure t
+  :after lsp-mode)
+
+(use-package yasnippet
+  :ensure t
+  :config (yas-global-mode 1))
+
+(use-package company
+  :ensure t
+  :hook (prog-mode . company-mode))
+
+(use-package dap-mode
+  :ensure t
+  :config
+  ;; Enable dap-ui and dap-mode globally or for specific modes
+  (dap-ui-mode 1)
+  (dap-mode 1))
 
 ;; Python configuration
 (use-package elpy
@@ -50,8 +156,10 @@
   :config
   (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "-i --simple-prompt")
+  (setq elpy-test-runner 'elpy-test-pytest-runner)
+  (setq elpy-test-pytest-runner-command '("pytest" "-sv"))
   (elpy-enable))
-;;(remove-hook 'elpy-modules 'elpy-module-flymake)
+(remove-hook 'elpy-modules 'elpy-module-flymake)
 
 ;; Used by pipenv!
 (use-package pyvenv
@@ -69,20 +177,27 @@
 ;;    #'pipenv-projectile-after-switch-extended))
 
 ;; Org mode configuration
-(use-package org)
-(add-hook 'org-mode-hook (lambda() (org-superstar-mode 1)))
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
-(setq org-startup-indented t)
-(setq org-duration-format (quote h:mm)) ;; display hours in clock table
-(use-package org-pomodoro)
-(use-package org-superstar)
+(use-package org
+  :ensure t
+  :hook (org-mode . org-superstar-mode)
+  :bind (("C-c l" . org-store-link)
+         ("C-c a" . org-agenda))
+  :config
+  (setq org-log-done t)
+  (setq org-startup-indented t)
+  (setq org-duration-format 'h:mm))
 
+(use-package org-pomodoro
+  :ensure t
+  :hook (org-mode . org-superstar-mode))
+
+(use-package org-superstar
+  :ensure t)
 
 ;; TODO look at vertico and consult
 ;; which-key
 (use-package which-key
+  :ensure t
   :config
   (which-key-mode 1))
 
@@ -111,76 +226,69 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 (setq next-line-add-newlines t)
 
-;; Packages I like
-(setq package-list '(markdown-mode paredit rainbow-delimiters rust-mode org-journal company magit pipenv))
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-;; Paredit
-;; (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-;; (add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
-;; (add-hook 'ielm-mode-hook 'enable-paredit-mode)
-;; (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
-;; (add-hook 'lisp-mode-hook 'enable-paredit-mode)
-
-;; Rainbows
-(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'lisp-interaction-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
-(require 'rainbow-delimiters)
-(set-face-foreground 'rainbow-delimiters-depth-1-face "#c66")  ; red
-(set-face-foreground 'rainbow-delimiters-depth-2-face "#6c6")  ; green
-(set-face-foreground 'rainbow-delimiters-depth-3-face "#69f")  ; blue
-(set-face-foreground 'rainbow-delimiters-depth-4-face "#cc6")  ; yellow
-(set-face-foreground 'rainbow-delimiters-depth-5-face "#6cc")  ; cyan
-(set-face-foreground 'rainbow-delimiters-depth-6-face "#c6c")  ; magenta
-(set-face-foreground 'rainbow-delimiters-depth-7-face "#ccc")  ; light gray
-(set-face-foreground 'rainbow-delimiters-depth-8-face "#999")  ; medium gray
-(set-face-foreground 'rainbow-delimiters-depth-9-face "#666")  ; dark gray
-
-;; Custom commands
-(defun show-current-time ()
-  "Show current time for two seconds."
-  (interactive)
-  (message (current-time-string))
-  (sleep-for 2)
-  (message nil))
-(global-set-key (kbd "C-c t") 'show-current-time)
-
-;; Window navigation (sort of like tmux's)
-(use-package switch-window
+;; Use-package based configuration for packages you like
+(use-package markdown-mode
   :ensure t)
-(global-set-key (kbd "C-x o") 'switch-window)
-(global-set-key (kbd "C-x 1") 'switch-window-then-maximize)
-(global-set-key (kbd "C-x 2") 'switch-window-then-split-below)
-(global-set-key (kbd "C-x 3") 'switch-window-then-split-right)
-(global-set-key (kbd "C-x 0") 'switch-window-then-delete)
 
-(global-set-key (kbd "C-x 4 d") 'switch-window-then-dired)
-(global-set-key (kbd "C-x 4 f") 'switch-window-then-find-file)
-(global-set-key (kbd "C-x 4 m") 'switch-window-then-compose-mail)
-(global-set-key (kbd "C-x 4 r") 'switch-window-then-find-file-read-only)
+(use-package paredit
+  :ensure t)
 
-(global-set-key (kbd "C-x 4 C-f") 'switch-window-then-find-file)
-(global-set-key (kbd "C-x 4 C-o") 'switch-window-then-display-buffer)
+(use-package rainbow-delimiters
+  :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode) ;; Enable rainbow-delimiters in programming modes
+  :config
+  ;; Custom colors for rainbow-delimiters
+  (set-face-foreground 'rainbow-delimiters-depth-1-face "#c66")  ; red
+  (set-face-foreground 'rainbow-delimiters-depth-2-face "#6c6")  ; green
+  (set-face-foreground 'rainbow-delimiters-depth-3-face "#69f")  ; blue
+  (set-face-foreground 'rainbow-delimiters-depth-4-face "#cc6")  ; yellow
+  (set-face-foreground 'rainbow-delimiters-depth-5-face "#6cc")  ; cyan
+  (set-face-foreground 'rainbow-delimiters-depth-6-face "#c6c")  ; magenta
+  (set-face-foreground 'rainbow-delimiters-depth-7-face "#ccc")  ; light gray
+  (set-face-foreground 'rainbow-delimiters-depth-8-face "#999")  ; medium gray
+  (set-face-foreground 'rainbow-delimiters-depth-9-face "#666")) ; dark gray
 
-(global-set-key (kbd "C-x 4 0") 'switch-window-then-kill-buffer)
+(use-package rust-mode
+  :ensure t)
+
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status))) ;; Bind magit-status to C-x g
+
+;; Winner Mode for quickly switching back after new windows pop open
+(winner-mode 1)
+
+;; Window navigation with switch-window
+(use-package switch-window
+  :ensure t
+  :bind (("C-x o" . switch-window)
+         ("C-x 1" . switch-window-then-maximize)
+         ("C-x 2" . switch-window-then-split-below)
+         ("C-x 3" . switch-window-then-split-right)
+         ("C-x 0" . switch-window-then-delete)
+         ("C-x 4 d" . switch-window-then-dired)
+         ("C-x 4 f" . switch-window-then-find-file)
+         ("C-x 4 m" . switch-window-then-compose-mail)
+         ("C-x 4 r" . switch-window-then-find-file-read-only)
+         ("C-x 4 C-f" . switch-window-then-find-file)
+         ("C-x 4 C-o" . switch-window-then-display-buffer)
+         ("C-x 4 0" . switch-window-then-kill-buffer)))
 
 
-;; Language servers
-;;(add-to-list 'elglot-server-programs '(html-mode . ("
-
-;; Server-client stuff
-(require 'server)
-(unless (server-running-p)
-  (server-start))
+;; Ensure the Emacs server is running
+(use-package server
+  :ensure nil  ;; `server` is built-in, no need to ensure
+  :config
+  (unless (server-running-p)
+    (server-start)))
 
 ;; Projectile
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("C-c p" . projectile-command-map)))
 
 ;; Treemacs
 (use-package treemacs
@@ -209,36 +317,36 @@
   :ensure t)
 
 ;; Ivy/Counsel/Swiper
-(ivy-mode)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-;; enable this if you want `swiper' to use it
-;; (setq search-default-mode #'char-fold-to-regexp)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+(use-package ivy
+  :ensure t
+  :diminish
+  :bind (("C-c C-r" . ivy-resume)
+         ("C-x B" . ivy-switch-buffer-other-window))
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
+  (ivy-mode 1))
 
-(put 'upcase-region 'disabled nil)
+(use-package counsel
+  :ensure t
+  :bind (("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-r" . counsel-recentf)
+         ("C-c g" . counsel-git)
+         ("C-c j" . counsel-git-grep)
+         ("C-c k" . counsel-ag)
+         ("C-c l" . counsel-locate)))
 
-;; Recent files
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(setq recentf-max-saved-items 25)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(use-package counsel-projectile
+  :ensure t
+  :config
+  (counsel-projectile-mode))
+(use-package swiper
+  :ensure t
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)))
 
 ;; Misc crap
 (define-coding-system-alias 'UTF-8 'utf-8)
+(put 'upcase-region 'disabled nil)
